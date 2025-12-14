c679a07fa74f30a6dc60b1bfece5d6cd0cac5012
SpecificMachine1 <fjt72701@yahoo.com>
2025-12-14 16:04:03 -0500

    benchmark day 4

--- bench/profile.scm
+++ bench/profile.scm
@@ -2,6 +2,7 @@
         (prefix (day day01) day01-)
         (prefix (day day02) day02-)
         (prefix (day day03) day03-)
+        (prefix (day day04) day04-)
         (bench runner))
 (let ((runs 10)
       (units micro))
@@ -29,4 +30,12 @@
   (run "3.2 override joltage"
        runs
        units
-       (day03-total-overide-output (day03-get-data "../data/day03-input.dat"))))
+       (day03-total-overide-output (day03-get-data "../data/day03-input.dat")))
+  (run "4.1 first pass rolls"
+       runs
+       units
+       (day04-count-accessible (day04-get-data "../data/day04-input.dat") 4))
+  (run "4.2 all passes rolls"
+       runs
+       units
+       (day04-count-all-accessible (day04-get-data "../data/day04-input.dat") 4)))
